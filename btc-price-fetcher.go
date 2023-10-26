3bf74ff276b639faec77e94ef14498bf4141bb0d
Emalinuxawy <emalinuxawy@gmail.com>
2023-10-25 23:46:53 -0600

    Spliting the logic in different functions for more readability

--- btc-price-fetcher.go
+++ btc-price-fetcher.go
@@ -1,5 +1,3 @@
-// 1. make request to get the price
-// 2. open a csv file to save the price that i got.
 package main
 
 import (
@@ -26,16 +24,33 @@ type CoinData struct {
 	} `json:"data"`
 }
 
-func main() {
+func fetchAndWriteDataToCSV() {
+	// fetch data
+	data, err := fetchData()
+	if err != nil {
+		fmt.Println("Error fetching data:", err)
+	}
+
+	// write the data to a CSV file
+	err = writeDataToCSV(data, "btc_prices.csv")
+	if err != nil {
+		fmt.Println("Error writing data to CSV:", err)
+	}
+}
+
+func fetchData() (CoinData, error) {
 	uri := "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
 	client := &http.Client{}
+
+	// initiate http GET request againest the url
 	req, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 		os.Exit(1)
 	}
 
+	// set Query Parameters and HTTP Headers
 	q := url.Values{}
 	q.Add("start", "1")
 	q.Add("limit", "1")
@@ -44,57 +59,73 @@ func main() {
 	req.Header.Add("X-CMC_PRO_API_KEY", os.Getenv("COIN_MARKET_CAP_TOKEN"))
 	req.URL.RawQuery = q.Encode()
 
+	// sending the HTTP request
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error sending the request to CoinMarketCap!")
 		os.Exit(1)
 	}
+
 	respBody, _ := io.ReadAll(resp.Body)
 	var coinData CoinData
 	err = json.Unmarshal(respBody, &coinData)
 	if err != nil {
 		fmt.Println("Error unmarshaling JSON:", err)
-		return
+	}
+	return coinData, nil
+}
+
+func writeDataToCSV(data CoinData, filename string) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Can't open the file", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Adding CSV header
+	columnName := []string{"Date", "BTC Price in USD"}
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println("Can't open the file", err)
+	}
+	// This will run if the file is not exist or is empty
+	isEmpty := fileInfo.Size() == 0
+	if isEmpty {
+		writer.Write(columnName)
+		if err != nil {
+			fmt.Println("Error writing columns: ", err)
+		}
 	}
 
-	parsedTime, err := time.Parse(time.RFC3339Nano, coinData.Status.Timestamp)
+	// convert the timestamp to human readable
+	parsedTime, err := time.Parse(time.RFC3339Nano, data.Status.Timestamp)
 	if err != nil {
 		fmt.Println("Failed to parse timestamp from API response.")
-		return
 	}
 
 	shortTime := parsedTime.Format("2006-01-02 15:04")
-	f, err := os.OpenFile("btc_prices.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println("Can't open the file", err)
-	}
-	defer f.Close()
 
-	w := csv.NewWriter(f)
-	defer w.Flush()
-	columnName := []string{"BTC Price in USD"}
-	fileInfo, err := f.Stat()
-	if err != nil {
-		fmt.Println("Can't open the file", err)
-		return
-	}
-	isEmpty := fileInfo.Size() == 0
-
-	if isEmpty {
-		w.Write(columnName)
-		if err != nil {
-			fmt.Println("Error writing columns: ", err)
-			return
-		}
+	for _, btc := range data.Data[0].Quote {
+		price := fmt.Sprintf("%.2f", btc.Price)
+		writer.Write([]string{shortTime, fmt.Sprintf("%.2f", btc.Price)})
+		fmt.Println("Date:", shortTime, "BTC Price: $", price)
 	}
 
-	if len(coinData.Data) > 0 {
-		for _, v := range coinData.Data[0].Quote {
-			price := fmt.Sprintf("%.2f", v.Price)
-			cprice := fmt.Sprint(price)
-			p := []string{fmt.Sprint(price)}
-			w.Write(p)
-			fmt.Println("Date:", shortTime, "BTC Price: $", cprice)
-		}
+	return nil
+}
+
+func main() {
+	// Create a ticker that trigger every 5 minutes
+	ticker := time.NewTicker(5 * time.Minute)
+
+	// invoke the initial function
+	fetchAndWriteDataToCSV()
+
+	// Run script every 5 minutes
+	for range ticker.C {
+		fetchAndWriteDataToCSV()
 	}
 }
